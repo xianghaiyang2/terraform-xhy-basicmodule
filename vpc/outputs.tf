134b698d5edb31256ea3ea903b76089e6ab3c550
sfadf <sadfsadf>
2020-12-28 17:12:50 +0800

    sfsd

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -6,11 +6,11 @@ output "vpc_cidr_block" {
   value = "${alicloud_vpc.vpc.0.cidr_block}"
 }
 
-output "vswitch_ids" {
-  value = "${alicloud_vswitch.vswitch.*.id}"
-}
-
-output "availability_zones" {
-  value = "${alicloud_vswitch.vswitch.*.availability_zone}"
-}
+//output "vswitch_ids" {
+//  value = "${alicloud_vswitch.vswitch.*.id}"
+//}
+//
+//output "availability_zones" {
+//  value = "${alicloud_vswitch.vswitch.*.availability_zone}"
+//}
 
