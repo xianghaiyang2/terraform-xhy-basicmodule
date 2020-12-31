fdbc1416b29e62b7719df513f260e12ecd277991
sfadf <sadfsadf>
2020-12-31 20:38:06 +0800

    该BUG

--- db/mongo/instance.tf
+++ db/mongo/instance.tf
@@ -10,7 +10,7 @@ resource "alicloud_mongodb_instance" "mongodb" {
   instance_charge_type = "${var.instance_charge_type}"                   # 付费类型
   vswitch_id           = element(distinct(compact(concat(var.vswitch_ids))), 0)   # 交换机id， 这里的方式和ecs实例一样
   account_password     = "${var.account_password}"                          # 密码
-  security_ip_list     = "${var.security_ip_list[0]}"                       # 白名单,这里方案是设置成vpc的ip网段
+  security_ip_list     = ["${var.security_ip_list}"]                       # 白名单,这里方案是设置成vpc的ip网段
 
   backup_time          = "${var.backup_time}"
   backup_period        = "${var.backup_period}"
