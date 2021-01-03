fa3fb154ccbfc11b0b8dbafca576705e8ddc194a
sfadf <sadfsadf>
2021-01-03 23:43:58 +0800

    截止到eip的相对完美版本

--- slb/output.tf
+++ slb/output.tf
@@ -1,7 +1,7 @@
 output "slb_id" {
-  value = "${data.alicloud_slbs.slb.slbs.0.id}"
+  value = alicloud_slb.slb.*.id
 }
 
 output "slb_address" {
-  value = "${data.alicloud_slbs.slb.slbs.0.address}"
+  value = alicloud_slb.slb.*.id
 }
