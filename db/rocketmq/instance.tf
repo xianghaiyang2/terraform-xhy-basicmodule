d3a8a86504a6fde78d9912eab3c248f0e7bc3350
sfadf <sadfsadf>
2021-01-05 17:48:33 +0800

    修改下标的BUG

--- db/rocketmq/instance.tf
+++ db/rocketmq/instance.tf
@@ -8,7 +8,7 @@ resource "alicloud_ons_instance" "rocketmq" {
 
 resource "alicloud_ons_group" "group" {
   count       = var.use_mq_module ? (var.mqtopic_count !=0 ? 2 : (var.delete_protection ? 2 : 0)) : 0
-  instance_id = alicloud_ons_instance.rocketmq.id          # 实例id
+  instance_id = alicloud_ons_instance.rocketmq.0.id          # 实例id
   group_id    = "${var.group_name}"-"${format(var.count_format, count.index+1)}"                            # 组名已经被group_name代替，一般命名为 "GID_"
   group_type  = element(distinct(compact(concat(var.group_type))), count.index)                           # 指定该组的协议（http, tcp）这里要轮循两者都要创建
   remark      = var.group_description                      # 组描述
@@ -17,7 +17,7 @@ resource "alicloud_ons_group" "group" {
 
 resource "alicloud_ons_topic" "topic" {
   count        = var.use_mq_module ? (var.mqtopic_count != 0 ? var.mqtopic_count : (var.delete_protection ? 1 : 0)) : 0
-  instance_id  = alicloud_ons_instance.rocketmq.id    # 实例id
+  instance_id  = alicloud_ons_instance.rocketmq.0.id    # 实例id
   topic        = "${var.topic_name}"-"${format(var.count_format, count.index+1)}"
   message_type = var.topic_message_type              # 消息类型 https://www.alibabacloud.com/help/zh/doc-detail/29591.html
   remark       = var.topic_description                 # topic描述
